c658f0c2bb59c61c04a8ace06a2aa5774c97f8c3
Khriizt <71673542+Khriizt@users.noreply.github.com>
2026-07-16 13:50:58 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Kraptor123/Cs-Karma")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Khriizt/CS-Repo2")
 
         authors = listOf("kraptor")
     }
