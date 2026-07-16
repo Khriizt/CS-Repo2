4ac30ba80ae26bb8754a58a7790422c12665eabb
Khriizt <71673542+Khriizt@users.noreply.github.com>
2026-07-16 13:53:40 -0600

    Update build.gradle.kts

--- Latanime/build.gradle.kts
+++ Latanime/build.gradle.kts
@@ -1,10 +1,10 @@
-// ! Bu araç @ByAyzen tarafından | @CS-Karma için yazılmıştır.
+// ! Esta herramienta fue escrita por | @MrRobiin.
 version = 8
 
 cloudstream {
-    authors     = listOf("ByAyzen")
+    authors     = listOf("MrRobiin")
     language    = "mx"
-    description = "odos los anime con Doblaje Latino/Castellano solo aquí en Latanime. Animes de estreno, subtitulado al español y con Audio Latino o Castellano, Online, HD y gratis. Para descargar por MEGA, Mediafire o Drive sin limites."
+    description = "Todos los anime con Doblaje Latino/Castellano solo aquí en Latanime. Animes de estreno, subtitulado al español y con Audio Latino o Castellano, Online, HD."
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movie") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://latanime.org/&size=16"
